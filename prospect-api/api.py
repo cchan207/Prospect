221de0dbfa58cafb2e55a3c4030227ac4e4bf302
Charlene Orr <charlorrnot@gmail.com>
2020-10-22 15:40:36 -0900

    API is working :)

--- prospect-api/api.py
+++ prospect-api/api.py
@@ -1,11 +1,8 @@
 from flask import Flask, jsonify, request
 from db import get_users
 
-app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def users():
-    return get_users()    
+    return get_users()
 
 if __name__ == '__main__':
-    app.run()
+    app.run()        Nav.Link href="/"> </Nav.Lik
