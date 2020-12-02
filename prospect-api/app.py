9f9202adffab07a50cb0c88ad39853dbd5240f7c
Cora Chan <corachan055@gmail.com>
2020-12-01 22:00:17 -0500

    Location apis

--- prospect-api/app.py
+++ prospect-api/app.py
@@ -66,6 +66,59 @@ class State(db.Model):
 	StateId = db.Column(db.Integer, primary_key = True, nullable = False)
 	StateName = db.Column(db.String(50), nullable = False)
 
+# Takes an application id, city name, and state name, adds location to applicationlocation
+# Can call this multiple times for applications with multiple locations
+# NOTE: Ensure that city name exists app side (reminder that city names can belong to multiple states)
+@app.route('/api/v1/add/locations', methods = ['POST'])
+def add_locations():
+	# get application id
+	appId = request.form.get('ApplicationId')
+	cityName = request.form.get('CityName')
+	stateName = request.form.get('StateName')
+
+	connection = engine.connect()
+	if appId:
+		try:
+			city_name_query = text(
+				'SELECT CityId FROM city WHERE CityName = :c_name;'
+			)
+			state_name_query = text(
+				'SELECT StateId FROM state WHERE StateName = :s_name;'
+			)
+			cityId = engine.execute(city_name_query, c_name = cityName).fetchone()
+			stateId = engine.execute(state_name_query, s_name = stateName).fetchone()
+
+			# make ApplicationLocation object
+			appLocation = Applicationlocation(
+				ApplicationId = appId,
+				CityId = cityId[0],
+				StateId = stateId[0]
+			)
+			# add to applicaton table
+			db.session.add(appLocation)
+			db.session.commit()
+
+			# response
+			responseObject = {
+				'status' : 'success',
+				'message': 'Sucessfully registered.'
+			}
+			return make_response(responseObject, 200)
+		except:
+			return make_response({
+				'status' : 'failed',
+				'message' : 'Some error occured !!'
+			})
+		finally:
+			connection.close()
+	else:
+		responseObject = {
+				'status' : 'fail',
+				'message': 'Missing location fields !!'
+		}
+		return make_response(responseObject, 400)
+
+# Takes in appliationId, returns city id, city name, state id, and state name
 @app.route('/api/v1/search/locations', methods = ['GET'])
 def get_locations():
 	# get application id
@@ -511,11 +564,11 @@ def get_applications():
 		}
 		return make_response(responseObject, 400)
 
-# Takes in UserId (can be found using search users api), CompanyId, PositionTitle, ApplicationLink, ApplicationStatus, and ApplicationDate
+# Takes in UserId (can be found using search users api), CompanyId, PositionTitle, ApplicationLink, ApplicationStatus, ApplicationDate
 # UserId, CompanyId, PositionTitle required, ApplicationLink can be NULL, ApplicationStatus will be default PENDING, ApplicationDate will be default now in utc
 # Note: Make sure CompanyId exists, if not create a new company app side
 @app.route('/api/v1/add/applications', methods = ['POST'])
-def add_applications():
+def add_application():
 	# geting name and email
 	userId = request.form.get('UserId')
 	companyId = request.form.get('CompanyId')
