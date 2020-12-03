96c2b18c5612e599ad90badb93633edd36cb66cb
shield13 <shield13@purdue.edu>
2020-12-03 18:47:41 -0500

    Added new text box fields, and the start of a state dropdown menu

--- prospect-app/src/pages/newjob.js
+++ prospect-app/src/pages/newjob.js
@@ -10,27 +10,68 @@ import AppContext from '../components/context';
 
 import Navbar from '../components/navbar-lp';
 
+
 export default function LandingPage() {
+      // Job Information
+    const [jobTitle, setJobTitle] = useState('');
+    const [company, setCompany] = useState('');
+    const [appLink, setAppLink] = useState('');
+      // Location Information
+    const [city, setCity] = useState('');
+      // Recruiter Information
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
-
+    const [phoneNumber, setPhoneNumber] = useState('');
+      
     useEffect(() => {
         window.scrollTo(0, 0)
     }, [])
 
     const activateButton = () => {
+          // Job info
+        console.log(jobTitle)
+        console.log(company)
+        console.log(appLink)
+          // Location info
+        console.log(city)
+          // Recruiter info
         console.log(firstName)
         console.log(lastName)
         console.log(email)
+        console.log(phoneNumber)
 
         const urlLink = "http://127.0.0.1:5000/api/v1/add/user";
 
-        const res = axios.post(urlLink, `FirstName=${firstName}&LastName=${lastName}&Email=${email}`);
+        const res = axios.post(urlLink, `JobTitle=${jobTitle}&Company=${company}&AppLink=${appLink}
+        &City=${city}
+        &FirstName=${firstName}&LastName=${lastName}&Email=${email}&PhoneNumber=${phoneNumber}`);
         return res.form;
 
     }
 
+      // States Dropdown
+
+
+      // Job Information
+    const handleCompany = (e) => {
+      setCompany(e.target.value);
+    }
+
+    const handleJobTitle = (e) => {
+      setJobTitle(e.target.value);
+    }
+
+    const handleAppLink = (e) => {
+      setAppLink(e.target.value);
+    }
+
+      // Location Information
+    const handleCity = (e) => {
+      setCity(e.target.value);
+    }
+
+      // Recruiter Information
     const handleFirstName = (e) => {
       setFirstName(e.target.value);
     }
@@ -42,24 +83,60 @@ export default function LandingPage() {
     const handleEmail = (e) => {
       setEmail(e.target.value);
     }
+      
+    const handlePhoneNumber = (e) => {
+      setPhoneNumber(e.target.value)
+    }
+      
 
     return (
       <div>
           <Navbar />
             <Container className="addUser-bg">
               <Form>
+              <h4>Job Information</h4>
+              <Form.Group onChange={handleCompany} value={company}>
+                  <Form.Label>Company</Form.Label>
+                  <Form.Control type="textarea" placeholder="Company Name" />
+              </Form.Group>
+              <Form.Group onChange={handleJobTitle} value={jobTitle}>
+                  <Form.Label>Job Title</Form.Label>
+                  <Form.Control type="textarea" placeholder="Job Title" />
+              </Form.Group>
+              <Form.Group onChange={handleAppLink} value={appLink}>
+                  <Form.Label>Application Link</Form.Label>
+                  <Form.Control type="textarea" placeholder="url" />
+              </Form.Group>
+              <h4>Location</h4>
+              <Form.Group onChange={handleCity} value={city}>
+                  <Form.Label>City</Form.Label>
+                  <Form.Control type="textarea" placeholder="City Name" />
+              </Form.Group>
+              <div class="dropdown">
+                <button class="dropbtn">Dropdown</button>
+                <div class="dropdown-content">
+                  <a href="#">Alaska</a>
+                  <a href="#">Arizona</a>
+                  <a href="#">Arkansas</a>
+                </div>
+              </div>
+              <h4>Recruiter Information</h4>
               <Form.Group onChange={handleFirstName} value={firstName}>
                   <Form.Label>First Name</Form.Label>
-                  <Form.Control type="textarea" placeholder="First Name" />
+                  <Form.Control type="textarea" placeholder="Jane" />
               </Form.Group>
               <Form.Group onChange={handleLastName} value={lastName}>
                   <Form.Label>Last Name</Form.Label>
-                  <Form.Control type="textarea" placeholder="Last Name" />
+                  <Form.Control type="textarea" placeholder="Doe" />
               </Form.Group>
               <Form.Group onChange={handleEmail} value={email}>
                   <Form.Label>Email</Form.Label>
-                  <Form.Control type="textarea" placeholder="Email" />
+                  <Form.Control type="textarea" placeholder="Email@domain" />
               </Form.Group>
+              <Form.Group onChange={handlePhoneNumber} value={phoneNumber}>
+                  <Form.Label>Phone Number</Form.Label>
+                  <Form.Control type="textarea" placeholder="XXX-XXX-XXXX" />
+              </Form.Group>                                               
               </Form>
               <Button variant="primary" type="submit" onClick={activateButton}>SUBMIT</Button>
             </Container>
