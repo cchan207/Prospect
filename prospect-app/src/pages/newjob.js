002fbaffc1a6d62bb9329917b6ebd6dab42f96b4
shield13 <shield13@purdue.edu>
2020-12-04 13:11:29 -0500

    Have all the fields added, still need option to add recruiters/locations

--- prospect-app/src/pages/newjob.js
+++ prospect-app/src/pages/newjob.js
@@ -24,6 +24,11 @@ export default function LandingPage() {
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
+      // Application Information
+    const [appStatus, setAppStatus] = useState('');
+    const [appYear, setAppYear] = useState('');
+    const [appMonth, setAppMonth] = useState('');
+    const [appDay, setAppDay] = useState('');
       
     useEffect(() => {
         window.scrollTo(0, 0)
@@ -42,12 +47,18 @@ export default function LandingPage() {
         console.log(lastName)
         console.log(email)
         console.log(phoneNumber)
+          // Application Status
+        console.log(appStatus)
+        console.log(appYear)
+        console.log(appMonth)
+        console.log(appDay)
 
         const urlLink = "http://127.0.0.1:5000/api/v1/add/user";
 
         const res = axios.post(urlLink, `JobTitle=${jobTitle}&Company=${company}&AppLink=${appLink}
         &City=${city}&State=${state}
-        &FirstName=${firstName}&LastName=${lastName}&Email=${email}&PhoneNumber=${phoneNumber}`);
+        &FirstName=${firstName}&LastName=${lastName}&Email=${email}&PhoneNumber=${phoneNumber}
+        &AppStatus=${appStatus}&AppYear=${appYear}&AppMonth=${appMonth}&AppDay=${appDay}`);
         return res.form;
 
     }
@@ -93,6 +104,23 @@ export default function LandingPage() {
     const handlePhoneNumber = (e) => {
       setPhoneNumber(e.target.value)
     }
+      // App Information
+    const handleAppStatus = (e) => {
+      setAppStatus(e.target.value)
+    }
+
+    const handleAppYear = (e) => {
+      setAppYear(e.target.value)
+    }
+
+    const handleAppMonth = (e) => {
+      setAppMonth(e.target.value)
+    }
+
+    const handleAppDay = (e) => {
+      setAppDay(e.target.value)
+    }
+
       
 
     return (
@@ -172,6 +200,7 @@ export default function LandingPage() {
                   <option value="Wyoming">Wyoming</option>
                 </select>
               </Form.Group>
+              
               <h4>Recruiter Information</h4>
               <Form.Group onChange={handleFirstName} value={firstName}>
                   <Form.Label>First Name</Form.Label>
@@ -188,7 +217,98 @@ export default function LandingPage() {
               <Form.Group onChange={handlePhoneNumber} value={phoneNumber}>
                   <Form.Label>Phone Number</Form.Label>
                   <Form.Control type="textarea" placeholder="XXX-XXX-XXXX" />
-              </Form.Group>                                               
+              </Form.Group> 
+              <h4>Application Information</h4>
+              <Form.Group onChange={handleAppStatus} value={appStatus}>
+                  <Form.Label>Application Status:</Form.Label>
+                  <select id="state" name="state">
+                    <option value="PENDING">PENDING</option>
+                    <option value="ACCEPTED">ACCEPTED</option>
+                    <option value="DENIED">DENIED</option>
+                  </select>
+              </Form.Group> 
+              <h6>Enter Date:</h6>
+              <Form.Group onChange={handleAppYear} value={appYear}>
+                  <Form.Label>Year:</Form.Label>
+                  <select id="appYear" name="appYear">
+                    <option value="2010">2010</option>
+                    <option value="2011">2011</option>
+                    <option value="2012">2012</option>
+                    <option value="2013">2013</option>
+                    <option value="2014">2014</option>
+                    <option value="2015">2015</option>
+                    <option value="2016">2016</option>
+                    <option value="2017">2017</option>
+                    <option value="2018">2018</option>
+                    <option value="2019">2019</option>
+                    <option value="2020">2020</option>
+                    <option value="2021">2021</option>
+                    <option value="2022">2022</option>
+                    <option value="2023">2023</option>
+                    <option value="2024">2024</option>
+                    <option value="2025">2025</option>
+                    <option value="2026">2026</option>
+                    <option value="2027">2027</option>
+                    <option value="2028">2028</option>
+                    <option value="2029">2029</option>
+                    <option value="2030">2030</option>
+                  </select>
+              </Form.Group> 
+              <Form.Group onChange={handleAppMonth} value={appMonth}>
+                  <Form.Label>Month:</Form.Label>
+                  <select id="appMonth" name="appMonth">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    
+                  </select>
+              </Form.Group>    
+              <Form.Group onChange={handleAppDay} value={appDay}>
+                  <Form.Label>Day:</Form.Label>
+                  <select id="appDay" name="appDay">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                    <option value="21">21</option>
+                    <option value="22">22</option>
+                    <option value="23">23</option>
+                    <option value="24">24</option>
+                    <option value="25">25</option>
+                    <option value="26">26</option>
+                    <option value="27">27</option>
+                    <option value="28">28</option>
+                    <option value="29">29</option>
+                    <option value="30">30</option>
+                    <option value="31">31</option>
+                    <option value="32">32</option>
+                  </select>
+              </Form.Group>                                        
               </Form>
               <Button variant="primary" type="submit" onClick={activateButton}>SUBMIT</Button>
             </Container>
