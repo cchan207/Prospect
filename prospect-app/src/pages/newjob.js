d1866d85225fd7bf48eaf6369634b388c9e83d36
shield13 <shield13@purdue.edu>
2020-12-03 19:31:54 -0500

    all 50 states in select box

--- prospect-app/src/pages/newjob.js
+++ prospect-app/src/pages/newjob.js
@@ -18,6 +18,7 @@ export default function LandingPage() {
     const [appLink, setAppLink] = useState('');
       // Location Information
     const [city, setCity] = useState('');
+    const [state, setState] = useState('');
       // Recruiter Information
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
@@ -35,6 +36,7 @@ export default function LandingPage() {
         console.log(appLink)
           // Location info
         console.log(city)
+        console.log(state)
           // Recruiter info
         console.log(firstName)
         console.log(lastName)
@@ -44,7 +46,7 @@ export default function LandingPage() {
         const urlLink = "http://127.0.0.1:5000/api/v1/add/user";
 
         const res = axios.post(urlLink, `JobTitle=${jobTitle}&Company=${company}&AppLink=${appLink}
-        &City=${city}
+        &City=${city}&State=${state}
         &FirstName=${firstName}&LastName=${lastName}&Email=${email}&PhoneNumber=${phoneNumber}`);
         return res.form;
 
@@ -69,6 +71,10 @@ export default function LandingPage() {
       // Location Information
     const handleCity = (e) => {
       setCity(e.target.value);
+    }
+
+    const handleState = (e) => {
+      setState(e.target.value);
     }
 
       // Recruiter Information
@@ -112,14 +118,60 @@ export default function LandingPage() {
                   <Form.Label>City</Form.Label>
                   <Form.Control type="textarea" placeholder="City Name" />
               </Form.Group>
-              <div class="dropdown">
-                <button class="dropbtn">Dropdown</button>
-                <div class="dropdown-content">
-                  <a href="#">Alaska</a>
-                  <a href="#">Arizona</a>
-                  <a href="#">Arkansas</a>
-                </div>
-              </div>
+              <Form.Group onChange={handleState} value={state}>
+                <label for="States">State:</label>
+                <select id="state" name="state">
+                  <option value="Alabama">Alabama</option>
+                  <option value="Alaska">Alaska</option>
+                  <option value="Arizona">Arizona</option>
+                  <option value="Arkansas">Arkansas</option>
+                  <option value="California">California</option>
+                  <option value="Colorado">Colorado</option>
+                  <option value="Connecticut">Connecticut</option>
+                  <option value="Delaware">Delaware</option>
+                  <option value="Florida">Florida</option>
+                  <option value="Georgia">Georgia</option>
+                  <option value="Hawaii">Hawaii</option>
+                  <option value="Idaho">Idaho</option>
+                  <option value="Illinois">Illinois</option>
+                  <option value="Indiana">Indiana</option>
+                  <option value="Iowa">Iowa</option>
+                  <option value="Kansas">Kansas</option>
+                  <option value="Kentucky">Kentucky</option>
+                  <option value="Louisiana">Louisiana</option>
+                  <option value="Maine">Maine</option>
+                  <option value="Maryland">Maryland</option>
+                  <option value="Massachusetts">Massachusetts</option>
+                  <option value="Michigan">Michigan</option>
+                  <option value="Minnesota">Minnesota</option>
+                  <option value="Mississippi">Mississippi</option>
+                  <option value="Missouri">Missouri</option>
+                  <option value="Montana">Montana</option>
+                  <option value="Nebraska">Nebraska</option>
+                  <option value="Nevada">Nevada</option>
+                  <option value="New Hampshire">New Hampshire</option>
+                  <option value="New Jersey">New Jersey</option>
+                  <option value="New Mexico">New Mexico</option>
+                  <option value="New York">New York</option>
+                  <option value="North Carolina">North Carolina</option>
+                  <option value="North Dakota">North Dakota</option>
+                  <option value="Ohio">Ohio</option>
+                  <option value="Oklahoma">Oklahoma</option>
+                  <option value="Pennsylvania">Pannsylvania</option>
+                  <option value="Rhode Island">Rhode Island</option>
+                  <option value="South Carolina">South Carolina</option>
+                  <option value="South Dakota">South Dakota</option>
+                  <option value="Tennessee">Tennessee</option>
+                  <option value="Texas">Texas</option>
+                  <option value="Utah">Utah</option>
+                  <option value="Vermont">Vermont</option>
+                  <option value="Virginia">Virginia</option>
+                  <option value="Washington">Washington</option>
+                  <option value="West Virginia">West Virginia</option>
+                  <option value="Wisconsin">Wisconsin</option>
+                  <option value="Wyoming">Wyoming</option>
+                </select>
+              </Form.Group>
               <h4>Recruiter Information</h4>
               <Form.Group onChange={handleFirstName} value={firstName}>
                   <Form.Label>First Name</Form.Label>
