474e77392a8afc54c32f485ed8d7199c0cd9dc31
Cora Chan <corachan055@gmail.com>
2020-12-07 20:08:18 -0500

    Consolidated form fields

--- prospect-app/src/pages/newjob.js
+++ prospect-app/src/pages/newjob.js
@@ -159,210 +159,97 @@ const handleAddClick = () => {
 
     return (
       <div>
-          <Navbar />
-            <Container className="addUser-bg">
-              <Form>
-              <h4>Job Information</h4>
-              <Form.Group onChange={handleCompany} value={company}>
-                  <Form.Label>Company</Form.Label>
-                  <Form.Control type="textarea" placeholder="Company Name" />
-              </Form.Group>
-              <Form.Group onChange={handleJobTitle} value={jobTitle}>
-                  <Form.Label>Job Title</Form.Label>
-                  <Form.Control type="textarea" placeholder="Job Title" />
-              </Form.Group>
-              <Form.Group onChange={handleAppLink} value={appLink}>
-                  <Form.Label>Application Link</Form.Label>
-                  <Form.Control type="textarea" placeholder="url" />
-              </Form.Group>
-              <h4>Location</h4>
-              
-              {locationList.map((x, i) => {
-                return (
-                  <div className="box">
-                    
-                    <Form.Group onChange={e => handleLocationChange(e, i)} value={x.City}>
-                      <Form.Label>City</Form.Label>
-                      <Form.Control type="textarea" placeholder="City Name" />
-                    </Form.Group>
-                    
-                    <Form.Group onChange={e => handleLocationChange(e, i)} value={x.State}>
-                      <label for="States">State:</label>
-                      <select id="state" name="state">
-                        <option value="Alabama">Alabama</option>
-                        <option value="Alaska">Alaska</option>
-                        <option value="Arizona">Arizona</option>
-                        <option value="Arkansas">Arkansas</option>
-                        <option value="California">California</option>
-                        <option value="Colorado">Colorado</option>
-                        <option value="Connecticut">Connecticut</option>
-                        <option value="Delaware">Delaware</option>
-                        <option value="Florida">Florida</option>
-                        <option value="Georgia">Georgia</option>
-                        <option value="Hawaii">Hawaii</option>
-                        <option value="Idaho">Idaho</option>
-                        <option value="Illinois">Illinois</option>
-                        <option value="Indiana">Indiana</option>
-                        <option value="Iowa">Iowa</option>
-                        <option value="Kansas">Kansas</option>
-                        <option value="Kentucky">Kentucky</option>
-                        <option value="Louisiana">Louisiana</option>
-                        <option value="Maine">Maine</option>
-                        <option value="Maryland">Maryland</option>
-                        <option value="Massachusetts">Massachusetts</option>
-                        <option value="Michigan">Michigan</option>
-                        <option value="Minnesota">Minnesota</option>
-                        <option value="Mississippi">Mississippi</option>
-                        <option value="Missouri">Missouri</option>
-                        <option value="Montana">Montana</option>
-                        <option value="Nebraska">Nebraska</option>
-                        <option value="Nevada">Nevada</option>
-                        <option value="New Hampshire">New Hampshire</option>
-                        <option value="New Jersey">New Jersey</option>
-                        <option value="New Mexico">New Mexico</option>
-                        <option value="New York">New York</option>
-                        <option value="North Carolina">North Carolina</option>
-                        <option value="North Dakota">North Dakota</option>
-                        <option value="Ohio">Ohio</option>
-                        <option value="Oklahoma">Oklahoma</option>
-                        <option value="Pennsylvania">Pannsylvania</option>
-                        <option value="Rhode Island">Rhode Island</option>
-                        <option value="South Carolina">South Carolina</option>
-                        <option value="South Dakota">South Dakota</option>
-                        <option value="Tennessee">Tennessee</option>
-                        <option value="Texas">Texas</option>
-                        <option value="Utah">Utah</option>
-                        <option value="Vermont">Vermont</option>
-                        <option value="Virginia">Virginia</option>
-                        <option value="Washington">Washington</option>
-                        <option value="West Virginia">West Virginia</option>
-                        <option value="Wisconsin">Wisconsin</option>
-                        <option value="Wyoming">Wyoming</option>
-                </select>
-              </Form.Group>
+      <Navbar />
+      <Container className="addUser-bg">
+        <Form>
+        <Form.Group>
 
-                  <div className="btn-box">
-                    {locationList.length !== 1 && <button
-                      className="mr10"
-                      onClick={() => handleRemoveClick(i)}>Remove</button>}
-                    {locationList.length - 1 === i && <button onClick={handleAddClick}>Add</button>}
-                  </div>
-               </div>
-              );
-              })}
-              
-              <h4>Recruiter Information</h4>
-              <Form.Group onChange={handleFirstName} value={firstName}>
-                  <Form.Label>First Name</Form.Label>
-                  <Form.Control type="textarea" placeholder="Jane" />
-              </Form.Group>
-              <Form.Group onChange={handleLastName} value={lastName}>
-                  <Form.Label>Last Name</Form.Label>
-                  <Form.Control type="textarea" placeholder="Doe" />
-              </Form.Group>
-              <Form.Group onChange={handleEmail} value={email}>
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control type="textarea" placeholder="Email@domain" />
-              </Form.Group>
-              <Form.Group onChange={handlePhoneNumber} value={phoneNumber}>
-                  <Form.Label>Phone Number</Form.Label>
-                  <Form.Control type="textarea" placeholder="XXX-XXX-XXXX" />
-              </Form.Group> 
-              <h4>Application Information</h4>
-              <Form.Group onChange={handleAppStatus} value={appStatus}>
-                  <Form.Label>Application Status:</Form.Label>
-                  <select id="state" name="state">
-                    <option value="PENDING">PENDING</option>
-                    <option value="ACCEPTED">ACCEPTED</option>
-                    <option value="DENIED">DENIED</option>
-                  </select>
-              </Form.Group> 
-              <h6>Enter Date:</h6>
-              <Form.Group onChange={handleAppYear} value={appYear}>
-                  <Form.Label>Year:</Form.Label>
-                  <select id="appYear" name="appYear">
-                    <option value="2010">2010</option>
-                    <option value="2011">2011</option>
-                    <option value="2012">2012</option>
-                    <option value="2013">2013</option>
-                    <option value="2014">2014</option>
-                    <option value="2015">2015</option>
-                    <option value="2016">2016</option>
-                    <option value="2017">2017</option>
-                    <option value="2018">2018</option>
-                    <option value="2019">2019</option>
-                    <option value="2020">2020</option>
-                    <option value="2021">2021</option>
-                    <option value="2022">2022</option>
-                    <option value="2023">2023</option>
-                    <option value="2024">2024</option>
-                    <option value="2025">2025</option>
-                    <option value="2026">2026</option>
-                    <option value="2027">2027</option>
-                    <option value="2028">2028</option>
-                    <option value="2029">2029</option>
-                    <option value="2030">2030</option>
-                  </select>
-              </Form.Group> 
-              <Form.Group onChange={handleAppMonth} value={appMonth}>
-                  <Form.Label>Month:</Form.Label>
-                  <select id="appMonth" name="appMonth">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                    
-                  </select>
-              </Form.Group>    
-              <Form.Group onChange={handleAppDay} value={appDay}>
-                  <Form.Label>Day:</Form.Label>
-                  <select id="appDay" name="appDay">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                    <option value="13">13</option>
-                    <option value="14">14</option>
-                    <option value="15">15</option>
-                    <option value="16">16</option>
-                    <option value="17">17</option>
-                    <option value="18">18</option>
-                    <option value="19">19</option>
-                    <option value="20">20</option>
-                    <option value="21">21</option>
-                    <option value="22">22</option>
-                    <option value="23">23</option>
-                    <option value="24">24</option>
-                    <option value="25">25</option>
-                    <option value="26">26</option>
-                    <option value="27">27</option>
-                    <option value="28">28</option>
-                    <option value="29">29</option>
-                    <option value="30">30</option>
-                    <option value="31">31</option>
-                    <option value="32">32</option>
-                  </select>
-              </Form.Group>                                        
-              </Form>
-              <Button variant="primary" type="submit" onClick={activateButton}>SUBMIT</Button>
-            </Container>
-      </div>
+        <h4>JOB INFORMATION</h4>
+        <Form.Label onChange={handleJobTitle}>Job Title</Form.Label>
+        <Form.Control type="textarea" placeholder="Job Title" />
+        <Form.Label onChange={handleAppLink}>Application URL</Form.Label>
+        <Form.Control type="textarea" placeholder="Application URL" />
+        <Form.Label>Application Status:</Form.Label>
+        <Form.Control as="select">
+            <option value="PENDING">PENDING</option>
+            <option value="ACCEPTED">ACCEPTED</option>
+            <option value="DENIED">DENIED</option>
+        </Form.Control>
+
+        <h4>COMPANY INFORMATION</h4>
+        <Form.Label onChange={handleCompany}>Company Name</Form.Label>
+        <Form.Control type="textarea" placeholder="Company Name" />
+        <Form.Label>City</Form.Label>
+        <Form.Control type="textarea" placeholder="City" />
+      
+        <Form.Label>State</Form.Label>
+        <Form.Control as="select">
+            <option value="Alabama">Alabama</option>
+            <option value="Alaska">Alaska</option>
+            <option value="Arizona">Arizona</option>
+            <option value="Arkansas">Arkansas</option>
+            <option value="California">California</option>
+            <option value="Colorado">Colorado</option>
+            <option value="Connecticut">Connecticut</option>
+            <option value="Delaware">Delaware</option>
+            <option value="Florida">Florida</option>
+            <option value="Georgia">Georgia</option>
+            <option value="Hawaii">Hawaii</option>
+            <option value="Idaho">Idaho</option>
+            <option value="Illinois">Illinois</option>
+            <option value="Indiana">Indiana</option>
+            <option value="Iowa">Iowa</option>
+            <option value="Kansas">Kansas</option>
+            <option value="Kentucky">Kentucky</option>
+            <option value="Louisiana">Louisiana</option>
+            <option value="Maine">Maine</option>
+            <option value="Maryland">Maryland</option>
+            <option value="Massachusetts">Massachusetts</option>
+            <option value="Michigan">Michigan</option>
+            <option value="Minnesota">Minnesota</option>
+            <option value="Mississippi">Mississippi</option>
+            <option value="Missouri">Missouri</option>
+            <option value="Montana">Montana</option>
+            <option value="Nebraska">Nebraska</option>
+            <option value="Nevada">Nevada</option>
+            <option value="New Hampshire">New Hampshire</option>
+            <option value="New Jersey">New Jersey</option>
+            <option value="New Mexico">New Mexico</option>
+            <option value="New York">New York</option>
+            <option value="North Carolina">North Carolina</option>
+            <option value="North Dakota">North Dakota</option>
+            <option value="Ohio">Ohio</option>
+            <option value="Oklahoma">Oklahoma</option>
+            <option value="Pennsylvania">Pannsylvania</option>
+            <option value="Rhode Island">Rhode Island</option>
+            <option value="South Carolina">South Carolina</option>
+            <option value="South Dakota">South Dakota</option>
+            <option value="Tennessee">Tennessee</option>
+            <option value="Texas">Texas</option>
+            <option value="Utah">Utah</option>
+            <option value="Vermont">Vermont</option>
+            <option value="Virginia">Virginia</option>
+            <option value="Washington">Washington</option>
+            <option value="West Virginia">West Virginia</option>
+            <option value="Wisconsin">Wisconsin</option>
+            <option value="Wyoming">Wyoming</option>
+        </Form.Control>
+
+        <h4>RECRUITER INFORMATION</h4>
+        <Form.Label onChange={handleFirstName} value={firstName}>First Name</Form.Label>
+        <Form.Control type="textarea" placeholder="First Name" />
+        <Form.Label onChange={handleLastName} value={lastName}>Last Name</Form.Label>
+        <Form.Control type="textarea" placeholder="Last Name" />
+        <Form.Label onChange={handleEmail} value={email}>Email</Form.Label>
+        <Form.Control type="textarea" placeholder="Email" />
+        <Form.Label onChange={handlePhoneNumber} value={phoneNumber}>Phone Number</Form.Label>
+        <Form.Control type="textarea" placeholder="xxx-xxx-xxxx" />
+
+        </Form.Group>
+        </Form>
+        <Button variant="dark" type="submit" onClick={activateButton}>SUBMIT</Button>
+    </Container>
+    </div>
     )
 }
 
